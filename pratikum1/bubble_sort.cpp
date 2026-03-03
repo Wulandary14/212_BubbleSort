a2162a430c55bd73d79545e8bf9bbfa3ee951f3c
Wulandary14 <Moonywulan14@gmail.com>
2026-03-03 10:54:03 +0700

    commit2

---
+++ pratikum1/bubble_sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20]; //deklarasi variable global array a dengan ukuran 20
+int n; //deklarasi variable global n untuk menyimpan banyaknya elemen pada array
+
+void input(){
+    //procedur untuk input
+    int d;
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array : "; //output ke layar
+        cin >> n;                                          //input dari pengguna
+        if (n <= 20)                                       //jika n kurang dari atau sama dengan 20
+        
+    }
+}
