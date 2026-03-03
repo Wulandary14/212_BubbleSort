fcc6628b53bb351570e7c403bc88ca4e76a26da7
Wulandary14 <Moonywulan14@gmail.com>
2026-03-03 11:13:16 +0700

    declare variable, add procedure input

--- pratikum1/bubble_sort.cpp
+++ pratikum1/bubble_sort.cpp
@@ -13,6 +13,20 @@ void input(){
         cout << "masukkan banyaknya elemen pada array : "; //output ke layar
         cin >> n;                                          //input dari pengguna
         if (n <= 20)                                       //jika n kurang dari atau sama dengan 20
-        
+            break;                                         //keluar dari loop
+        else
+        {                                                           //jia n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";//output ke layar
+        }
+    }
+    cout << endl;
+    cout << "===================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "===================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {                                           //looping dengan i dimulai dari 0 hingga n-1
+        cout << "data ke-" << (i+1) << ": ";    //output ke layar
+        cin >> arr[i];
     }
 }
